2cf9c1212b4674175da5357b9bf91b3b99ee02e4
Ayan Kumar Pan <ayankumarpan@gmail.com>
2023-09-16 12:07:35 +0530

    Add files via upload

---
+++ boomiUrlEncode.groovy
@@ -0,0 +1,10 @@
+import java.net.URLEncoder
+
+// Input URL containing special characters
+String inputUrl = inputValue;
+
+// Perform full URL encoding
+String encodedUrl = URLEncoder.encode(inputUrl, "UTF-8");
+
+// Print the encoded URL
+encodedValue = encodedUrl;
